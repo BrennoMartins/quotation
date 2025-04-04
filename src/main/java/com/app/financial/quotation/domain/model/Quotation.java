10be8032d04c665515e105ec2ef97d8eaa75c5d5
Brenno Martins <brennomartins@hotmail.com>
2025-04-04 20:50:37 -0300

    version CRUD quotation

--- src/main/java/com/app/financial/quotation/domain/model/Quotation.java
+++ src/main/java/com/app/financial/quotation/domain/model/Quotation.java
@@ -1,15 +1,12 @@
 package com.app.financial.quotation.domain.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,4 +17,5 @@ public class Quotation {
     private String name;
     private BigDecimal value;
     private LocalDateTime dateLastUpdate = LocalDateTime.now();
+    private boolean automaticUpdateValue;
 }
